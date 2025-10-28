e787d3d38ba9d061d52f38ba3d6ad99db152995b
akash <akash9122000@gmail.com>
2025-10-28 17:39:39 +0530

    created logic for the face detection and manipulation, work in progress

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+from services.face_detector import FaceDetector
+from services.blur_detector import BlurDetector
+from services.image_processor import ImageProcessor
+
+__all__ = ['FaceDetector', 'BlurDetector', 'ImageProcessor']
